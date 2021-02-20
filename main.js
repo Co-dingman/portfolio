24a0a4b7fffaccf1c6eaf9de36fbfb33e6d2fcc8
cwpark <cwpark@tesna.co.kr>
2021-02-20 13:21:15 +0900

    Arrow up botton

--- main.js
+++ main.js
@@ -23,22 +23,47 @@ navbarMenu.addEventListener('click', (event) => {
     if(link === null){
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    console.log(navbarMenu.classList);
+    navbarMenu.classList.toggle('open');
 })
+
 
 // Home contact button
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () =>{
     scrollIntoView('#contact');
-})
-
-function scrollIntoView(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior : 'smooth'});
-}
+});
 
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
-})
+});
+
+// Show "arrow up" button
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.classList.add('visible');
+    }else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    console.log('click');
+    scrollIntoView('#home');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : 'smooth'});
+}
